e65cdaeec18705388f83afcad4f1e27139b86a74
ocoronamx <ocorona.desarrollo@gmail.com>
2024-11-27 21:13:07 -0600

    Implements admin-lte assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,10 @@ Rails.application.config.assets.version = "1.0"
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
 
+# AdminLTE 3 assets
+Rails.application.config.assets.paths << Rails.root.join("node_modules/admin-lte")
+Rails.application.config.assets.precompile += %w[dist/css/* dist/js/*.js]
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
